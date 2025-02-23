6aded74d397eff8e35d990357ff4bc6b64996280
Minyeong Jeong <megormin@gmail.com>
2025-02-23 14:19:32 +0100

    wip except experimental features, PPR

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,17 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
-  const { views: totalViews } = await client
+  const { views: totalViews } = (await client
     .withConfig({ useCdn: false })
-    .fetch(STARTUP_VIEWS_QUERY, { id });
+    .fetch(STARTUP_VIEWS_QUERY, { id })) || { views: 0 };
+
+  await writeClient
+    .patch(id)
+    .set({ views: totalViews ?? 0 + 1 })
+    .commit();
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -31,5 +31,11 @@ export const STARTUP_BY_ID_QUERY =
     pitch
 }`);
 
-export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type=="startup" && _id==$id][0]{
+export const STARTUP_VIEWS_QUERY =
+  defineQuery(`*[_type=="startup" && _id==$id][0]{
   _id,views}`);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY =
+  defineQuery(`*[_type=="author" && id==$id][0]{
+    _id,id, name, username,email, image, bio
+  }`);
